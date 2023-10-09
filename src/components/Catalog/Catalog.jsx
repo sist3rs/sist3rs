5206ae4d5c8c19859b796272b5602108dfcec78a
Sist3rs Shop <145418450+sist3rs@users.noreply.github.com>
2023-10-09 12:22:09 +0200

    Update Catalog.jsx

--- src/components/Catalog/Catalog.jsx
+++ src/components/Catalog/Catalog.jsx
@@ -151,7 +151,7 @@ const Catalog = () => {
       </h2>
        <p class="catalogo-link"><a href="https://photos.google.com/share/AF1QipODeyyXUrgw2Ymspo8rj46Ep11UMrkFgtYfMeN44ZWkd60L0x-_AqGUGTGn0-UWfA?key=NHNpdzRNZEI2OGtmY2RoVzFZODZtWU1JLS1rS3Rn" target="_blank">¡Visitá nuestro catálogo actualizado!</a>
        </p>
-      </br>
+      <br />
       <p>
       Nuestros medios de pago son Mercado pago, efectivo, transferencia y tarjetas bancarias con hasta 3 cuotas sin interés.   
       </p>
