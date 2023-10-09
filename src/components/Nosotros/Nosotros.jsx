72430f7ca881595149dc21525c3ced95a47a29db
Sist3rs Shop <145418450+sist3rs@users.noreply.github.com>
2023-10-09 09:57:17 +0200

    Update Nosotros.jsx
    Corrección texto

--- src/components/Nosotros/Nosotros.jsx
+++ src/components/Nosotros/Nosotros.jsx
@@ -30,10 +30,8 @@ Estamos comprometidas con desafiar la hegemonía de cuerpos y los estereotipos. 
         </div>
         <div className="nosotros-text-container">
           <h3>#todasSomosModelos</h3>
-          <p>
-            Somos Gise y Jime, hermanas y socias!
-Te ofrecemos múltiples opciones en moda urbana de calidad, para que encuentres dentro de tu estilo, la mejor versión de vos. 
-Estamos comprometidas con desafiar la hegemonía de cuerpos y los estereotipos. Celebramos la diversidad y promovemos la identificación de la belleza en cada mujer. .
+          <p>Te  proponemos un espacio acogedor, y el mejor asesoramiento para que te sientas a gusto, cómoda y hermosa eligiendo los looks que más te representen.
+Adaptamos  la experiencia de compra a la medida de tus necesidades, seleccionando colecciones versátiles para que explotes tu belleza y personalidad a través de las prendas que elegís para mostrarte al mundo.
           </p>
         </div>
       </article>
