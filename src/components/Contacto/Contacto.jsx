ea170ba48489bae8ef7eb4e68076563dcafacbec
Sist3rs Shop <145418450+sist3rs@users.noreply.github.com>
2023-10-09 11:29:05 +0200

    Update Contacto.jsx

--- src/components/Contacto/Contacto.jsx
+++ src/components/Contacto/Contacto.jsx
@@ -57,11 +57,12 @@ export const Contacto = () => {
 
   return (
     <div id="contacto" className="container-fluid  d-flex rectangle">
+      <div className="text-container">
       <iframe
         className="mapa-sisters"
         src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d13401.857098037988!2d-68.8585011!3d-32.9245551!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x967e0933018c4e95%3A0x28992c03e356a88a!2sSIST3RS!5e0!3m2!1sen!2sus!4v1638206714149!5m2!1sen!2sus"></iframe>
 
-      <div className="text-container">
+      
         <h2 className="title">Dónde Estamos</h2>
         <p className="address">
           ALTE. BROWN 1520, GODOY CRUZ, MENDOZA <br />
